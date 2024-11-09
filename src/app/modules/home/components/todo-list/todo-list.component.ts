de19d4e69db71a0d65de1ede1c58ce12944b42d5
Everton Teixeira <evertontp5@gmail.com>
2024-11-09 02:43:44 -0300

    criaçaõ da inserção de tarefas e validações para não ter tarefas vazias ou com espaços em branco

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -7,16 +7,18 @@ import { TaskList } from '../../interface/taskList.interface';
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
-  public taskList: TaskList[] = [
-    {
-      task: 'Task 1',
-      checked: true
-    }
-  ];
+  public taskList: TaskList[] = [];
 
   public deleteItem(event: number):void {
     this.taskList.splice(event, 1);
   }
+
+  public setEmitTask(event:string){
+    this.taskList.push({
+      task: event,
+      checked: false
+    });
+  };
 
   public deleteAll(){
     const confirmDialog = window.confirm('Are you sure you want to delete all tasks?');
